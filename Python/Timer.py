8e6d3b5b76d5297ff1c043e78f6c56f4d044874c
Abed Nashif <70801871+abednashif@users.noreply.github.com>
2022-03-06 14:24:30 +0200

    Update Timer.py

--- Python/Timer.py
+++ Python/Timer.py
@@ -10,16 +10,3 @@ def countdown(time_sec):
             time_sec -= 1
      print("Timer Ended!")
 countdown(100)
-
-# import turtle
-
-# colors = ['yellow','white','green', 'red','cyan' , 'blue']
-# t = turtle. Turtle()
-# screen = turtle.Screen()
-# screen. bgcolor('Black')
-# t. speed(30)
-# for i in range(100):
-#      t.color(colors[i%6])
-#      t.fd(i*5)
-#      t.left(100)
-#      t. width(2)
